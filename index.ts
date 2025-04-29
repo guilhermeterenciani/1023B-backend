fa18115fdf5762618133138133fa5729832b39e4
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-04-29 09:17:18 -0300

    Código de listar e cadastrar front-end e back-end.

--- index.ts
+++ index.ts
@@ -52,18 +52,27 @@ app.post("/estudantes", async (request: FastifyRequest, reply: FastifyReply) => 
         reply.status(200).send(dados)
         
     } catch (erro:any) {
-        if (erro.code === "ECONNREFUSED") {
-            console.log("ERRO: LIGUE O LARAGÃO!!! CABEÇA!")
-            reply.status(400).send({mensagem:"ERRO: LIGUE O LARAGÃO!!! CABEÇA!"})
-        } else if (erro.code === "ER_BAD_DB_ERROR") {
-            console.log("ERRO: CONFIRA O NOME DO BANCO DE DADOS OU CRIE UM NOVO BANCO COM O NOME QUE VOCÊ COLOCOU LÁ NA CONEXÃO")
-            reply.status(400).send({mensagem:"ERRO: CONFIRA O NOME DO BANCO DE DADOS OU CRIE UM NOVO BANCO COM O NOME QUE VOCÊ COLOCOU LÁ NA CONEXÃO"})
-        } else if (erro.code === "ER_ACCESS_DENIED_ERROR") {
-            console.log("ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO")
-            reply.status(400).send({mensagem:"ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO"})
-        } else {
-            console.log(erro)
-            reply.status(400).send({mensagem:"ERRO DESCONHECIDO OLHE O TERMINAL"})
+        switch (erro.code) {
+            case "ECONNREFUSED":
+                console.log("ERRO: LIGUE O LARAGÃO!!! CABEÇA!");
+                reply.status(400).send({ mensagem: "ERRO: LIGUE O LARAGÃO!!! CABEÇA!" });
+                break;
+            case "ER_BAD_DB_ERROR":
+                console.log("ERRO: CONFIRA O NOME DO BANCO DE DADOS OU CRIE UM NOVO BANCO COM O NOME QUE VOCÊ COLOCOU LÁ NA CONEXÃO");
+                reply.status(400).send({ mensagem: "ERRO: CONFIRA O NOME DO BANCO DE DADOS OU CRIE UM NOVO BANCO COM O NOME QUE VOCÊ COLOCOU LÁ NA CONEXÃO" });
+                break;
+            case "ER_ACCESS_DENIED_ERROR":
+                console.log("ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO");
+                reply.status(400).send({ mensagem: "ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO" });
+                break;
+            case "ER_DUP_ENTRY":
+                console.log("ERRO: VOCÊ DUPLICOU A CHAVE PRIMÁRIA");
+                reply.status(400).send({ mensagem: "ERRO: VOCÊ DUPLICOU A CHAVE PRIMÁRIA" });
+                break;
+            default:
+                console.log(erro);
+                reply.status(400).send({ mensagem: "ERRO DESCONHECIDO OLHE O TERMINAL DO BACKEND" });
+                break;
         }
     }
 })
